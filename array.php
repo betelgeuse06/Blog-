732bc19b7c393e649881d15831a20df4c946397a
betelgeuse06 <32902105+betelgeuse06@users.noreply.github.com>
2018-09-30 12:44:48 +0900

    PHP Blog

---
+++ array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+      $arrayName = array('kang', 'min', 'kyu');
+      echo $arrayNamep[0].'<br>';
+      echo $arrayNamep[1].'<br>';
+      echo $arrayNamep[2].'<br>';
+    ?>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+      $str = 1;
+      $a = 0;
+      while ($a <= 10) {
+        echo "1<br>";
+        $a++;
+      }
+    ?>
+  </body>
+</html>
